db20475ec8bc5ada489cd3ff4adb207b2c9bb112
mjilani98 <mjilani@emich.edu>
2025-04-02 12:48:37 -0400

    GameTimerTask.java : created : variables (Game game , GameView gameView) , public constructor , run() method

--- app/src/main/java/com/example/hw4q4/GameTimerTask.java
+++ app/src/main/java/com/example/hw4q4/GameTimerTask.java
@@ -1,4 +1,32 @@
 package com.example.hw4q4;
 
-public class GameTimerTask {
+import android.app.GameManager;
+
+public class GameTimerTask
+{
+    //make Game and GameView
+    private Game game ;
+    private GameView gameView;
+
+    //public constructor
+    public GameTimerTask(Game game , GameView gameView)
+    {
+        //set game and gameView
+        this.game = game;
+        this.gameView = gameView;
+    }
+
+
+    //method runs the view
+    public void run()
+    {
+        //move all 20 shapes
+        for(int x =0 ; x<game.getShapes().length ; x++)
+        {
+            game.getShapes()[x].move();;
+        }
+        
+        gameView.postInvalidate();
+    }
+
 }
