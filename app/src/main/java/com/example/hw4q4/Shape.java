ddda4cb77afa181a87a323942889e6cf9c1efe0a
mjilani98 <mjilani@emich.edu>
2025-04-02 10:40:59 -0400

    Shape.java : Added - local vairables , public constructor , move() method

--- app/src/main/java/com/example/hw4q4/Shape.java
+++ app/src/main/java/com/example/hw4q4/Shape.java
@@ -1,4 +1,59 @@
 package com.example.hw4q4;
 
-public class Shape {
+import android.graphics.Color;
+
+public class Shape
+{
+    //data for each shape
+    public int type;
+    public int centerX , centerY;
+    public int size;
+    public int speed;
+    public int direction;
+    public int color;
+
+    //public Constructor
+    public Shape()
+    {
+        //Initialize type of shape , (circle  =1, square =2)
+        type = (int) (Math.random()*2)+1 ; // random number between 1 , 2
+
+        //Initialize centerX and centerY (location on board)
+        centerX = (int) (Math.random() * 2001); // random number between 0 , 2000
+        centerY = (int) (Math.random() * 2001); // random number between 0 , 2000
+
+        //Initialize size of the shape
+        size = (int) (Math.random() * 51) + 50; // random number between 50 , 100
+
+        //Initialize speed of the shape
+        speed = (int) (Math.random() * 11) + 10; // random number between 10 , 20
+
+        //Initialize the direction of shape
+        direction = (int) (Math.random()*4)+1; // random number between 1 , 4
+
+        //Initialize random color for the shape
+        color = Color.rgb((int) (Math.random() * 256) , (int) (Math.random() * 256) , (int) (Math.random() * 256));
+    }
+
+    //method moves the shape based on the direction
+    public void move()
+    {
+        if(direction == 1)
+        {
+            centerX = (centerX + speed)%2000;
+        }
+        else if (direction == 2)
+        {
+            centerX = (centerX - speed)%2000;
+        }
+        else if (direction == 3)
+        {
+            centerY = (centerY + speed)%2000;
+        }
+        else if (direction == 4)
+        {
+            centerY = (centerY - speed)%2000;
+        }
+    }
+
 }
