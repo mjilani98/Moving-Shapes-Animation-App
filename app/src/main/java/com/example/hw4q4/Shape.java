d016c6f497ebbf12164ed3e1b77f2e21fdb817e2
mjilani98 <mjilani@emich.edu>
2025-04-02 15:33:11 -0400

    Shape.java : Editied speeds for direction 2 and 4

--- app/src/main/java/com/example/hw4q4/Shape.java
+++ app/src/main/java/com/example/hw4q4/Shape.java
@@ -2,6 +2,8 @@ package com.example.hw4q4;
 
 import android.graphics.Color;
 
+import java.util.Random;
+
 public class Shape
 {
     //data for each shape
@@ -15,6 +17,7 @@ public class Shape
     //public Constructor
     public Shape()
     {
+        Random random = new Random();
         //Initialize type of shape , (circle  =1, square =2)
         type = (int) (Math.random()*2)+1 ; // random number between 1 , 2
 
@@ -26,7 +29,7 @@ public class Shape
         size = (int) (Math.random() * 51) + 50; // random number between 50 , 100
 
         //Initialize speed of the shape
-        speed = (int) (Math.random() * 11) + 10; // random number between 10 , 20
+        speed = (int)(Math.random()*11)+10; // random number between 10 , 20
 
         //Initialize the direction of shape
         direction = (int) (Math.random()*4)+1; // random number between 1 , 4
@@ -44,7 +47,7 @@ public class Shape
         }
         else if (direction == 2)
         {
-            centerX = (centerX - speed)%2000;
+            centerX = (centerX - speed +2000)%2000;
         }
         else if (direction == 3)
         {
@@ -52,7 +55,7 @@ public class Shape
         }
         else if (direction == 4)
         {
-            centerY = (centerY - speed)%2000;
+            centerY = (centerY - speed+2000)%2000;
         }
     }
 
