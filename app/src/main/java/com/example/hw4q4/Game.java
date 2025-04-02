e964be63391d1731d15c81f8211be78dd34ecbfe
mjilani98 <mjilani@emich.edu>
2025-04-02 11:51:14 -0400

    Game.java : create : public contructor , move() method , getShapes() method

--- app/src/main/java/com/example/hw4q4/Game.java
+++ app/src/main/java/com/example/hw4q4/Game.java
@@ -1,4 +1,37 @@
 package com.example.hw4q4;
 
-public class Game {
+public class Game
+{
+    //array of shapes
+    private Shape[] shapesArray;
+    //number of arrays
+    int numOfShapes;
+
+    //public constructor
+    public Game()
+    {
+        //number of shapes
+        numOfShapes = 20;
+
+        //create array of 20 shapes
+        for(int x =0 ;  x < numOfShapes ; x++)
+        {
+            shapesArray[x] = new Shape();
+        }
+    }
+
+    //method moves the shapes
+    public void move()
+    {
+        for(int x = 0 ;  x< numOfShapes ; x++)
+        {
+            shapesArray[x].move();
+        }
+    }
+
+    //method return the array of shapes
+    public Shape[] getShapes()
+    {
+        return shapesArray;
+    }
 }
