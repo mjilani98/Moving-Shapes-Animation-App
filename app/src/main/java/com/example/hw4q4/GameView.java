5b1455bb98c3d57cc6f0bb4370f5927b64e58152
mjilani98 <mjilani@emich.edu>
2025-04-02 14:54:24 -0400

    GameView.java : set background color to black edited drawRec

--- app/src/main/java/com/example/hw4q4/GameView.java
+++ app/src/main/java/com/example/hw4q4/GameView.java
@@ -2,6 +2,7 @@ package com.example.hw4q4;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -10,7 +11,7 @@ public class GameView extends View
     //create game
     private Game game;
     //create a paint brush
-    private Paint paint;
+    private Paint paint = new Paint();;
 
     //public constructor
     public GameView(Context context , Game game)
@@ -27,6 +28,10 @@ public class GameView extends View
     //method displays view of game
     public void onDraw(Canvas canvas)
     {
+        //set background color black
+        paint.setColor(Color.parseColor("#000000"));
+        canvas.drawRect(0, 0, 10000, 10000, paint);
+
         //get array of shapes
         Shape[] shape = game.getShapes();
 
@@ -41,9 +46,18 @@ public class GameView extends View
                 canvas.drawCircle(shape[x].centerX, shape[x].centerY , shape[x].size,paint);
             //if type is 2 draw square using centerx, centery, side=2*size
             else if (shape[x].type == 2)
-                canvas.drawRect(shape[x].centerX , shape[x].centerY , shape[x].size*2 , shape[x].size*2 , paint);
-        }
+            {
+                //setting square coordinate
+                float left = shape[x].centerX - shape[x].size;
+                float top = shape[x].centerY - shape[x].size;
+                float right = shape[x].centerX + shape[x].size;
+                float bottom = shape[x].centerY + shape[x].size;
 
+                //draw square
+                canvas.drawRect(left, top, right, bottom, paint);
+            }
+
+        }
 
     }
 }
